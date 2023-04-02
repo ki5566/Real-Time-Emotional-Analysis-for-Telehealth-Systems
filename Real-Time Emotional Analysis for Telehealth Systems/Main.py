5852856d41d541345bf51c0c48a989a147c30cf2
ki5566 <106714647+ki5566@users.noreply.github.com>
2023-04-02 08:23:41 -0500

    Update Main.py

--- Real-Time Emotional Analysis for Telehealth Systems/Main.py
+++ Real-Time Emotional Analysis for Telehealth Systems/Main.py
@@ -1,3 +1,15 @@
+# Credits:
+# @misc{serengil2023db,
+# author       = {Serengil, Sefik Ilkin and Ozpinar, Alper},
+# title        = {An Evaluation of SQL and NoSQL Databases for Facial Recognition Pipelines},
+# year         = {2023},
+# publisher    = {Cambridge Open Engage},
+# doi          = {10.33774/coe-2023-18rcn},
+# url          = {https://doi.org/10.33774/coe-2023-18rcn},
+# howpublished = {https://www.cambridge.org/engage/coe/article-details/63f3e5541d2d184063d4f569},
+# note         = {Preprint}
+#      }
+
 import sys
 import random
 import datetime, time
